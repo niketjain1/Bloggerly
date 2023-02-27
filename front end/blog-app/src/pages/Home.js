219dd459731214eb5e9544389fd8fccaa9f79587
niketjain1 <niketj2000@gmail.com>
2023-02-27 23:28:25 +0530

    added front end files

---
+++ front end/blog-app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Base from "../components/Base";
+
+const Home=()=>{
+    return(
+        <Base>
+        <h1>This is home page</h1>
+        <p>Welcome to home page</p>
+        </Base>
+    )
+}
+
+export default Home;

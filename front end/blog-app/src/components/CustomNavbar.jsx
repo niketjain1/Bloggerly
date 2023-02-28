4234cdc09777a0703a019852cf74f5292ca403aa
niketjain1 <niketj2000@gmail.com>
2023-02-28 14:12:54 +0530

    Added Navbar

--- front end/blog-app/src/components/CustomNavbar.jsx
+++ front end/blog-app/src/components/CustomNavbar.jsx
@@ -1,3 +1,5 @@
+import { NavLink as ReactLink } from 'react-router-dom';
+
 import React, { useState } from 'react';
 import {
   Collapse,
@@ -23,35 +25,37 @@ const CustomNavbar = () => {
   return (
     <div>
       <Navbar 
-        className="my-2"
         color="dark"
         dark
+        expand="md"
+        fixed=''
       >
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand tag={ReactLink} to="/">My Blogs</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="me-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink tag={ReactLink} to="/about">About</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
+              <NavLink tag={ReactLink} to="/login">Login</NavLink>
+            </NavItem>
+            <NavItem> 
+              <NavLink tag={ReactLink} to="/signup">Signup</NavLink>
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
-                Options
+                More
               </DropdownToggle>
               <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem tag={ReactLink} to="/services">Services</DropdownItem>
+                <DropdownItem>Contact us</DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
+                <DropdownItem>LinkedIn</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
+          <NavbarText>Bloggerly</NavbarText>
         </Collapse>
       </Navbar>
     </div>
